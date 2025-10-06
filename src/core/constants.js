b71b6ad023336083599957cff0430144ab44a82e
Ioannis Giannakopoulos <ioannis.giannakopoulos@sword-group.com>
2025-10-06 12:08:19 +0300

    Try and Error

--- src/core/constants.js
+++ src/core/constants.js
@@ -1,3 +1,3 @@
 export const TROY_OUNCE_IN_GRAMS = 31.1034768;
 export const STORAGE_KEY = 'coinQuantities';
-export const CSV_FILE_PATH = '../../data/coins.csv';
+export const CSV_FILE_PATH = '../data/coins.csv';
